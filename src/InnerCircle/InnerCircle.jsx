56a253e85382992eacde1cb65f47621ca6b9bb62
Nicholas Weaver <nickqw14@gmail.com>
2019-10-17 12:37:43 -0600

    Created inner circle and fixed positioning on x axis

---
+++ src/InnerCircle/InnerCircle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles/innerCircle.module.scss";
+
+function InnerCircle() {
+	return <div className={styles.innerCircleContainer}></div>;
+}
+
+export default InnerCircle;
--- src/OuterCircle/OuterCircle.jsx
+++ src/OuterCircle/OuterCircle.jsx
@@ -3,10 +3,12 @@ import styles from "./styles/outerCircle.module.scss";
 
 import Xaxis from "./components/Xaxis";
 import Yaxis from "./components/Yaxis";
+import InnerCircle from "../InnerCircle/InnerCircle";
 
 function OuterCircle() {
 	return (
 		<div className={styles.outerCircleContainer}>
+			<InnerCircle />
 			<Xaxis />
 			<Yaxis />
 		</div>
